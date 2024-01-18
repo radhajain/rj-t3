469cf1a950bd83d4fb5810422c1a92b33bd87e8d
Radha Jain <jainr@stanford.edu>
2024-01-17 21:10:21 -0500

    t3 app

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from '@clerk/nextjs'
+import { api } from '~/trpc/server'
+
+export default async function Home() {
+  const allPosts = await api.post.getAll.query()
+
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white'>
+      <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16 '>
+        <h1 className='text-5xl font-extrabold tracking-tight sm:text-[5rem]'>
+          Create <span className='text-[hsl(280,100%,70%)]'>radha&apos;s</span>{' '}
+          App
+        </h1>
+        <UserButton afterSignOutUrl='/' />
+      </div>
+      {allPosts.map((post) => (
+        <div key={post.id}>
+          <span>{post.name}</span>
+          <div>{post.createdAt.toLocaleString()}</div>
+        </div>
+      ))}
+    </main>
+  )
+}
