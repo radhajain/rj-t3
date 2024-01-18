001f9da67565cac6c6d70f5ca5963eb734a27922
Radha Jain <jainr@stanford.edu>
2024-01-17 22:34:43 -0500

    tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,22 @@ export default async function Home() {
   const allPosts = await api.post.getAll.query()
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white'>
-      <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16 '>
+    <main className='flex justify-center h-screen'>
+      <div className='w-full md:max-w-6xl flex flex-col gap-5 items-center bg-red-100'>
         <h1 className='text-5xl font-extrabold tracking-tight sm:text-[5rem]'>
-          Create <span className='text-[hsl(280,100%,70%)]'>radha&apos;s</span>{' '}
-          App
+          Radha&apos;s Next App
         </h1>
         <UserButton afterSignOutUrl='/' />
+        {allPosts.map((post) => (
+          <div key={post.id}>
+            <div>{post.message}</div>
+            <div className='flex row gap-2'>
+              <b>{post.name}</b>
+              <div>{post.createdAt.toLocaleString()}</div>
+            </div>
+          </div>
+        ))}
       </div>
-      {allPosts.map((post) => (
-        <div key={post.id}>
-          <span>{post.name}</span>
-          <div>{post.createdAt.toLocaleString()}</div>
-        </div>
-      ))}
     </main>
   )
 }
